35fb2f6065ce0de7b18984284c486ad15465a542
Insight-Creator <luizbuchhalter@Outlook.com>
2020-09-30 16:20:26 -0300

    Dataset Challenge

---
+++ Dataset/lib/dataset.js
@@ -0,0 +1,27 @@
+const cast = (data) => {
+  if (data === "true") {
+    return true;
+  }
+
+  if (data === "false") {
+    return false;
+  }
+
+  if ((data.match(/\d/g) || []).length === data.length) {
+    return Number.parseInt(data, 10);
+  }
+  return data;
+};
+
+const dataset = (element) => {
+  const openingTag = element.split('>')[0];
+  const rawAttributes = openingTag.match(/data-\w+="\w+"/g) || [];
+  const attributes = {};
+  rawAttributes.forEach((attribute) => {
+    const matchData = attribute.match(/data-(.+)="(.+)"/);
+    attributes[matchData[1]] = cast(matchData[2]);
+  });
+  return attributes;
+};
+
+module.exports = dataset; // Do not remove.
